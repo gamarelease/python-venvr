edac7e1570c0032b05d54d7fff6f66bb60af6aec
Leonardo Gama <leonardo.gama@usp.br>
2023-06-06 13:07:42 -0300

    main entrypoint (python -m venvn)

---
+++ src/venvr/__main__.py
@@ -0,0 +1,12 @@
+import sys
+
+from venvr import main
+
+if __name__ == "__main__":
+    rc = 1
+    try:
+        main()
+        rc = 0
+    except Exception as e:
+        print("Error: %s" % e, file=sys.stderr)
+    sys.exit(rc)
